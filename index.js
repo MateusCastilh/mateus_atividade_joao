1a50fe126d9b5a6fc240113004a9e117b9ef0770
MateusCastilh <mateus.moraes.castilho@escola.pr.gov.br>
2026-03-25 09:11:43 -0300

    TRABALHO DE HOJE

---
+++ index.js
@@ -0,0 +1,31 @@
+///////////////////////////////////////////////
+
+let num1 = 15;
+let num2 = 27;
+let soma = num1 + num2;
+console.log("A soma é:", soma);
+
+///////////////////////////////////////////////
+
+let subtracao = 50 - 25;
+console.log("A subtração é:", subtracao);
+
+///////////////////////////////////////////////
+
+let multiplicacao = 8 * 12;
+console.log("A multiplicação é:", multiplicacao);
+
+///////////////////////////////////////////////
+
+let divisao = 100 / 4;
+console.log("A divisão é:", divisao);
+
+//////////////////////////////////////////////
+
+if (10==10) {
+console.log("O seu valor é igual a 10");
+}
+
+if (10!=10) {
+console.log("O seu valor não é igual a 10");
+}
