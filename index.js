d685880ab9e42aedf6f289ee88e492dc2f434d9e
MateusCastilh <mateus.moraes.castilho@escola.pr.gov.br>
2026-03-26 12:25:55 -0300

    TAREFA PRONTA JOÃO

--- index.js
+++ index.js
@@ -29,3 +29,49 @@ console.log("O seu valor é igual a 10");
 if (10!=10) {
 console.log("O seu valor não é igual a 10");
 }
+/////////////////////////////////////////////
+
+let nome1 = "João";
+let nome2 = "Maria";
+if (nome1 != nome2) {
+    console.log("O valor é: Os nomes são diferentes.");
+} else {
+    console.log("O valor é: Os nomes são iguais.");
+}
+
+/////////////////////////////////////////////
+
+if (5 < 10) {
+    console.log("O valor é: 5 é menor que 10.");
+} else {
+    console.log("O valor é: 5 não é menor que 10.");
+}
+
+/////////////////////////////////////////////
+
+let preco = 80;
+if (preco > 50) {
+    console.log("O valor é: O preço é maior que 50.");
+} else {
+    console.log("O valor é: O preço é 50 ou menos.");
+}
+
+/////////////////////////////////////////////
+
+let estoque = 20;
+if (estoque <= 20) {
+    console.log("O valor é: O estoque é menor ou igual a 20.");
+} else {
+    console.log("O valor é: O estoque é maior que 20.");
+}
+
+/////////////////////////////////////////////
+
+let idade = 18;
+if (idade >= 18) {
+    console.log("O valor é: A pessoa é maior de idade.");
+} else {
+    console.log("O valor é: A pessoa é menor de idade.");
+}
+
+/////////////////////////////////////////////
